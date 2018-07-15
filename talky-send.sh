96efb73bc22a719af4a2927fb754d157c4907d2e
Ibraheem Z. Abu-Kaff <ibraAbuKaff@users.noreply.github.com>
2018-07-15 18:05:25 +0400

    Update talky-send.sh

--- talky-send.sh
+++ talky-send.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-#to be implemented
+echo "Hello, Can we start talky-send? Y/N"
+
+read WANNA_OPEN_CHAT
+
+if [[ $WANNA_OPEN_CHAT != "Y" ]]; then
+	printf "\e[31m See You Later ~_~ "
+	printf "\033\e[0m"
+	exit;
+fi
+
+echo "Please provide the token:"
+read TOKEN
+
+echo 'Please provide the commands: e.g: pwd;echo "hi"; echo "done";OK'
+read COMMANDS
+
+responseToGetToken=$(curl --silent -X POST "https://api.keyvalue.xyz/$TOKEN/chatKey/$COMMANDS")
+
+echo "Commands have been send for execution \n"
